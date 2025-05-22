f2c55736543fef525311ff84327394a013246b2c
roger <minseungkim1017@gmail.com>
2025-05-22 22:30:10 +0900

    feat: keep working on set up

--- train_on_voc.py
+++ train_on_voc.py
@@ -1,21 +1,17 @@
 from ultralytics import YOLO
 
-# Load a pretrained YOLO11n model
 model = YOLO("yolo11n.pt")
 
-# Train the model on the COCO8 dataset for 100 epochs
 train_results = model.train(
-    data="coco8.yaml",  # Path to dataset configuration file
-    epochs=100,  # Number of training epochs
-    imgsz=640,  # Image size for training
-    device="cpu",  # Device to run on (e.g., 'cpu', 0, [0,1,2,3])
+    data="./datasets/VOC.yaml",  # dataset configuration file
+    epochs=10,  
+    imgsz=640,  
+    device="cuda", 
 )
 
-# Evaluate the model's performance on the validation set
 metrics = model.val()
 
-# Perform object detection on an image
-results = model("path/to/image.jpg")  # Predict on an image
-results[0].show()  # Display results
+results = model("./datasets/test_image.jpg")  # Predict on an image
+results[0].show()  
 
-path = model.export(format="onnx")  # Returns the path to the exported model
+# path = model.export(format="onnx")
