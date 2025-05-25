e9b5140ff66cc334c252252e452c81fa66b1615b
roger <minseungkim1017@gmail.com>
2025-05-25 23:42:02 +0900

    feat: finishing train python file

--- train.py
+++ train.py
@@ -1,21 +1,25 @@
 from ultralytics import YOLO
 
-model = YOLO("yolo11n.pt")
-
 # YAML Configuration file
 # data = "./datasets/VOC.yaml"
 data = "./datasets/VisDrone.yaml"
 
-train_results = model.train(
+model = YOLO("yolo11n.pt")
+print("\n-----------Validate Original Model-----------\n")
+metrics = model.val(data=data, plots=True)
+
+print("\n-----------Validate 50 Epoch Model-----------\n")
+# The best model after 50 Epoch
+trained_model = YOLO("yolo11n-trained-50.pt")
+metrics = trained_model.val(data=data, plots=True)
+
+# Train for 100 Epoch Model
+print("\n-----------Start Train-----------\n")
+train_results = trained_model.train(
     data=data,
-    epochs=50,  
+    epochs=1,  
     imgsz=640,  
     device="cuda", 
 )
 
-metrics = model.val()
-
-results = model("./datasets/test_image.jpg")
-results[0].show()  
-
-model.save("yolo11n-trained.pt")
+trained_model.save("yolo11n-trained-100.pt")
