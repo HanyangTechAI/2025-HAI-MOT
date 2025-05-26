4122197926761599abb88f28e5e1490e01316d43
roger <minseungkim1017@gmail.com>
2025-05-26 10:04:29 +0900

    feat: start 11s training

--- train_11n.py
+++ train_11n.py
@@ -4,14 +4,14 @@ from ultralytics import YOLO
 # data = "./datasets/VOC.yaml"
 data = "./datasets/VisDrone.yaml"
 
-# model = YOLO("yolo11n.pt")
-# print("\n-----------Validate Original Model-----------\n")
-# metrics = model.val(data=data, plots=True)
+model = YOLO("yolo11n.pt")
+print("\n-----------Validate Original Model-----------\n")
+metrics = model.val(data=data, plots=True)
 
-# print("\n-----------Validate 50 Epoch Model-----------\n")
-# # The best model after 50 Epoch
+print("\n-----------Validate 50 Epoch Model-----------\n")
+# The best model after 50 Epoch
 trained_model = YOLO("yolo11n-trained-50.pt")
-# metrics = trained_model.val(data=data, plots=True)
+metrics = trained_model.val(data=data, plots=True)
 
 # Train for 100 Epoch Model
 print("\n-----------Start Train-----------\n")
---
+++ train_11s.py
@@ -0,0 +1,19 @@
+from ultralytics import YOLO
+
+# YAML Configuration file
+data = "./datasets/VisDrone.yaml"
+
+model = YOLO("yolo11s.pt")
+print("\n-----------Validate Original Model-----------\n")
+metrics = model.val(data=data, plots=True)
+
+# Train for 100 Epoch Model
+print("\n-----------Start Train-----------\n")
+train_results = model.train(
+    data=data,
+    epochs=100, 
+    imgsz=640,  
+    device="cuda", 
+)
+
+model.save("yolo11s-trained-100.pt")
